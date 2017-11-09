4412574f45d3f72381f3a79feef11f82effffa66
Mikhail Bashkirov <bashmish@gmail.com>
2017-11-09 20:52:21 +0100

    [p] Add Polymer 1 support for open in editor feature

--- src/publisher/utils/custom-element.js
+++ src/publisher/utils/custom-element.js
@@ -38,12 +38,21 @@ export function getComponentFilePath(element) {
 }
 
 function isPolymerComponent(element) {
-  if (!window.Polymer || typeof window.Polymer.Element !== 'function') return false;
-  return element instanceof window.Polymer.Element;
+  return isPolymer1Component(element) || isPolymer2Component(element);
 }
 
 function getPolymerFilePath(element) {
   const dir = element.importPath.replace(window.location.origin, '');
   const filename = `${element.tagName.toLowerCase()}.html`;
   return `${dir}${filename}`;
+}
+
+function isPolymer1Component(element) {
+  // eslint-disable-next-line no-underscore-dangle
+  return element.__isPolymerInstance__;
+}
+
+function isPolymer2Component(element) {
+  if (!window.Polymer || typeof window.Polymer.Element !== 'function') return false;
+  return element instanceof window.Polymer.Element;
 }
